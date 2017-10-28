bace2200b030951915fb6d6f09ee290ee2da565c
Michelle Conway <32267382+mconwa02@users.noreply.github.com>
2017-10-28 13:04:04 +0100

    Updated final game code and ran it for test values contianed in the data.txt file

--- LunarLander.py
+++ LunarLander.py
@@ -1,4 +1,12 @@
-''' Michelle Conway MCONWA02'''
+''' Michelle Conway MCONWA02
+I created a program where you give your intial altitude from the sun, velocity which will be zero as
+movement just beings on spaceship and the amount of full you have. once enter the program run and asks
+if you want to continue landing spaceship using intital values, if you continue it tells you if you land or crash onto the moon,
+if you leave before landing you are thought you altitue and velocity and thanked for playing
+
+When entering varibable to run game use float values and anwser play again question with a character
+if you hit return it leaves game
+'''
 
 # altitude  = a  #metres
 # velocity  = v  #metres/second
@@ -34,7 +42,7 @@ def my_game(a,v,f):
                 s = input()
         else:
             print("Game Over")
-            if a >= 0:
+            if a >= 0 and f > 0:
                print("Your altitude is " + str(round(a1, 2)) + " metres above the moon")
                print("Your velocity is " + str(round(v, 2)) + " metres/second towards the moon")
                if f <= 0:
@@ -42,6 +50,11 @@ def my_game(a,v,f):
                else:
                    print("You have "+ str(round(f, 2)) +" litres of fuel reamaining")
                print("You have not landed on the moon!")
+            if a >= 0 and f <= 0:
+               print("Your altitude is " + str(round(a, 2)) + " metres above the moon")
+               print("Your velocity is " + str(round(v, 2)) + " metres/second towards the moon")
+               print("You have 0 litres of fuel reamaining")
+               print("You have not landed on the moon!")   
             elif a < 0 and v <= 10:
                print("Your altitude is 0 metres above the moon")
                print("Your velocity is " + str(round(v, 2)) + " metres/second towards the moon")
@@ -90,7 +103,7 @@ def my_game(a,v,f):
             s = input()
         else:
             if s[0] == 'y' or s[0] == 'Y':
-                while a > 0 and f >0:
+                while a > 0 and f > 0:
                     print("Your altitude is " + str(round(a, 2)) + " metres above the moon")
                     print("Your velocity is " + str(round(v, 2)) + " metres/second towards the moon")
                     print("You have "+ str(round(f, 2)) +" litres of fuel reamaining")
@@ -116,6 +129,11 @@ def my_game(a,v,f):
                        else:
                            print("You have "+ str(round(f, 2)) +" litres of fuel reamaining")
                        print("You have not landed on the moon!")
+                    if a >= 0 and f <= 0:
+                       print("Your altitude is " + str(round(a, 2)) + " metres above the moon")
+                       print("Your velocity is " + str(round(v, 2)) + " metres/second towards the moon")
+                       print("You have 0 litres of fuel reamaining")
+                       print("You have not landed on the moon!")    
                     elif a < 0 and v <= 10:
                        print("Your altitude is 0 metres above the moon")
                        print("Your velocity is " + str(round(v, 2)) + " metres/second towards the moon")
@@ -160,5 +178,5 @@ def my_game(a,v,f):
                 print("Thank you for playing, Goodbye!")
                     
 if __name__ == '__main__':
-    answer = my_game(1000.0, 0.0 , 1000.0)
+    answer = my_game(100.0, 0.0 , 100.0)
     print(answer)
