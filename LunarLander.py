52d02b7fa561e38e19823376bef3146c7f399e15
Michelle Conway <32267382+mconwa02@users.noreply.github.com>
2017-10-28 12:47:25 +0100

    Fixed the end game mesages and have a total of 3 loops to answer all answer combinations

--- LunarLander.py
+++ LunarLander.py
@@ -7,7 +7,7 @@
 import math 
 
 def my_game(a,v,f):
-    print("Your altitude is " + str(round(a, 2)) +" metres above the moon")
+    print("Your altitude is " + str(round(a, 2)) + " metres above the moon")
     print("Your velocity is " + str(round(v, 2)) + " metres/second towards the moon")
     print("You have "+ str(round(f, 2)) +" litres of fuel reamaining")
     print("Do you want to play?")
@@ -17,7 +17,7 @@ def my_game(a,v,f):
     s = input()
     if s[0] == 'y' or s[0] == 'Y':
         while a > 0 and f >0:
-            print("Your altitude is " + str(round(a, 2)) +" metres above the moon")
+            print("Your altitude is " + str(round(a, 2)) + " metres above the moon")
             print("Your velocity is " + str(round(v, 2)) + " metres/second towards the moon")
             print("You have "+ str(round(f, 2)) +" litres of fuel reamaining")
             print("Do you want to play again?")
@@ -27,37 +27,138 @@ def my_game(a,v,f):
                 a -= v
                 f -= 1
             elif s[0] == 'n' or s[0] == 'N':
+                a1 =a
                 a = 0
-                print("Thank you for playing, Goodbye!")
             else:
                 print("Do you want to play again?")
                 s = input()
         else:
             print("Game Over")
-            if a < 0 and v <= 10:
-                print("Your altitude is 0 metres above the moon")
-                print("Your velocity is " + str(round(v, 2)) + " metres/second towards the moon")
-                if f <= 0:
-                    print("You have 0 litres of fuel reamaining")
-                else:
-                    print("You have "+ str(round(f, 2)) +" litres of fuel reamaining")
-                print("Congratulation you have landed safely!")      
+            if a >= 0:
+               print("Your altitude is " + str(round(a1, 2)) + " metres above the moon")
+               print("Your velocity is " + str(round(v, 2)) + " metres/second towards the moon")
+               if f <= 0:
+                   print("You have 0 litres of fuel reamaining")
+               else:
+                   print("You have "+ str(round(f, 2)) +" litres of fuel reamaining")
+               print("You have not landed on the moon!")
+            elif a < 0 and v <= 10:
+               print("Your altitude is 0 metres above the moon")
+               print("Your velocity is " + str(round(v, 2)) + " metres/second towards the moon")
+               if f <= 0:
+                   print("You have 0 litres of fuel reamaining")
+               else:
+                   print("You have "+ str(round(f, 2)) + " litres of fuel reamaining")
+               print("Congratulation you have landed safely!")      
             elif a < 0 and v > 10:
-                print("You blasted a " + str(round(abs(a), 2)) + " meter crater in the moon!")
-                print("Your velocity is " + str(round(v, 2)) + " metres/second towards the moon")          
-                if f <= 0:
-                    print("You have 0 litres of fuel reamaining")
-                else:
-                    print("You have "+ str(round(f, 2)) +" litres of fuel reamaining")                
+               print("You blasted a " + str(round(abs(a), 2)) + " meter crater in the moon!")
+               print("Your velocity is " + str(round(v, 2)) + " metres/second towards the moon")          
+               if f <= 0:
+                   print("You have 0 litres of fuel reamaining")
+               else:
+                   print("You have "+ str(round(f, 2)) +" litres of fuel reamaining")
+            print("Thank you for playing, Goodbye!")
     elif s[0] == 'n' or s[0] == 'N':
-        print("Thank you for playing, Goodbye!")
+        print("Game Over")
+        if a >= 0:
+            print("Your altitude is " + str(round(a, 2)) + " metres above the moon")
+            print("Your velocity is " + str(round(v, 2)) + " metres/second towards the moon")
+            if f <= 0:
+                print("You have 0 litres of fuel reamaining")
+            else:
+                print("You have "+ str(round(f, 2)) +" litres of fuel reamaining")
+            print("You have not landed on the moon!")
+        elif a < 0 and v <= 10:
+            print("Your altitude is 0 metres above the moon")
+            print("Your velocity is " + str(round(v, 2)) + " metres/second towards the moon")
+            if f <= 0:
+                print("You have 0 litres of fuel reamaining")
+            else:
+                print("You have "+ str(round(f, 2)) + " litres of fuel reamaining")
+            print("Congratulation you have landed safely!")      
+        elif a < 0 and v > 10:
+            print("You blasted a " + str(round(abs(a), 2)) + " meter crater in the moon!")
+            print("Your velocity is " + str(round(v, 2)) + " metres/second towards the moon")          
+            if f <= 0:
+                print("You have 0 litres of fuel reamaining")
+            else:
+                print("You have "+ str(round(f, 2)) +" litres of fuel reamaining")
+            print("Thank you for playing, Goodbye!")
     else:
         while s[0] not in ['n', 'N', 'y', 'Y']:
             print("Do you want to play again?")
             s = input()
         else:
-            print("Game Over")
-            
+            if s[0] == 'y' or s[0] == 'Y':
+                while a > 0 and f >0:
+                    print("Your altitude is " + str(round(a, 2)) + " metres above the moon")
+                    print("Your velocity is " + str(round(v, 2)) + " metres/second towards the moon")
+                    print("You have "+ str(round(f, 2)) +" litres of fuel reamaining")
+                    print("Do you want to play again?")
+                    s = input()
+                    if s[0] == 'y' or s[0] == 'Y':
+                        v += 1.6 -0.15
+                        a -= v
+                        f -= 1
+                    elif s[0] == 'n' or s[0] == 'N':
+                        a1 = a
+                        a = 0
+                    else:
+                        print("Do you want to play again?")
+                        s = input()
+                else:
+                    print("Game Over")
+                    if a >= 0:
+                       print("Your altitude is " + str(round(a1, 2)) + " metres above the moon")
+                       print("Your velocity is " + str(round(v, 2)) + " metres/second towards the moon")
+                       if f <= 0:
+                           print("You have 0 litres of fuel reamaining")
+                       else:
+                           print("You have "+ str(round(f, 2)) +" litres of fuel reamaining")
+                       print("You have not landed on the moon!")
+                    elif a < 0 and v <= 10:
+                       print("Your altitude is 0 metres above the moon")
+                       print("Your velocity is " + str(round(v, 2)) + " metres/second towards the moon")
+                       if f <= 0:
+                           print("You have 0 litres of fuel reamaining")
+                       else:
+                           print("You have "+ str(round(f, 2)) + " litres of fuel reamaining")
+                       print("Congratulation you have landed safely!")      
+                    elif a < 0 and v > 10:
+                       print("You blasted a " + str(round(abs(a), 2)) + " meter crater in the moon!")
+                       print("Your velocity is " + str(round(v, 2)) + " metres/second towards the moon")          
+                       if f <= 0:
+                           print("You have 0 litres of fuel reamaining")
+                       else:
+                           print("You have "+ str(round(f, 2)) +" litres of fuel reamaining")
+                    print("Thank you for playing, Goodbye!")
+            elif s[0] == 'n' or s[0] == 'N':
+                print("Game Over")
+                if a >= 0:
+                    print("Your altitude is " + str(round(a, 2)) + " metres above the moon")
+                    print("Your velocity is " + str(round(v, 2)) + " metres/second towards the moon")
+                    if f <= 0:
+                        print("You have 0 litres of fuel reamaining")
+                    else:
+                        print("You have "+ str(round(f, 2)) +" litres of fuel reamaining")
+                    print("You have not landed on the moon!")
+                elif a < 0 and v <= 10:
+                    print("Your altitude is 0 metres above the moon")
+                    print("Your velocity is " + str(round(v, 2)) + " metres/second towards the moon")
+                    if f <= 0:
+                        print("You have 0 litres of fuel reamaining")
+                    else:
+                        print("You have "+ str(round(f, 2)) + " litres of fuel reamaining")
+                    print("Congratulation you have landed safely!")      
+                elif a < 0 and v > 10:
+                    print("You blasted a " + str(round(abs(a), 2)) + " meter crater in the moon!")
+                    print("Your velocity is " + str(round(v, 2)) + " metres/second towards the moon")          
+                    if f <= 0:
+                        print("You have 0 litres of fuel reamaining")
+                    else:
+                        print("You have "+ str(round(f, 2)) +" litres of fuel reamaining")
+                print("Thank you for playing, Goodbye!")
+                    
 if __name__ == '__main__':
     answer = my_game(1000.0, 0.0 , 1000.0)
     print(answer)
