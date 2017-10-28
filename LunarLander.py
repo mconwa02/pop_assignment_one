cd1c7c404edde1b5733dedfc05197a6c0827e943
Michelle Conway <32267382+mconwa02@users.noreply.github.com>
2017-10-28 11:29:13 +0100

    Added inital test calculations

--- LunarLander.py
+++ LunarLander.py
@@ -1,2 +1,24 @@
-'''You name should go here together with your login id.
-   Then your description of what the program does.'''
+''' Michelle Conway MCONWA02'''
+
+# altitude  = a  #metres
+# velocity  = v  #metres/second
+# fuel      = f  #Litres
+
+def my_game(a,v,f):
+    print("Your altitude is " + str(a) +" metres above the moon")
+    print("Your velocity is " + str(v) + " metres/second towards the moon")
+    print("You have "+ str(f) +" litres of fuel reamaining")
+    v += 1.6 
+    a -= v
+    f -= 1
+    while a > 0:
+        print("Your altitude is " + str(a) +" metres above the moon")
+        print("Your velocity is " + str(v) + " metres/second towards the moon")
+        print("You have "+ str(f) +" litres of fuel reamaining")            
+        v += 1.6 -0.15
+        a -= v
+        f -= 1
+
+if __name__ == '__main__':
+    answer = my_game(1000.0, 0.0 , 1000.0)
+    print(answer)
